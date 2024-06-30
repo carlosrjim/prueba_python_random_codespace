af0bcbc89bd762d5a6bfdbaf607e07156f9b7908
carlosrjim <56218432+carlosrjim@users.noreply.github.com>
2024-06-30 19:45:24 +0000

    carlos

--- prueba.py
+++ prueba.py
@@ -6,3 +6,9 @@ print("thanks for using the program")
 print("bye")
 r = "parece que funciona bien"
 #actualizado
+
+#programa random
+import random
+lista = ["a","b","c","d"]
+ale = random.randint(0,len(lista)-1)
+print("el nombre elegido es",lista[ale])
